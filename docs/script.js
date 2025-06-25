140fdfa09342c3d5dfb90ff730236ab2245e5b6c
Ben May <benmay@Bens-Air.communityfibre.co.uk>
2025-06-25 16:40:15 +0100

    updated json reference

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-fetch('https://raw.githubusercontent.com/bmaycops/DailySpanishWord/refs/heads/main/docs/words.json')
+fetch('https://raw.githubusercontent.com/bmaycops/DailySpanishWord/refs/heads/wordle-style/docs/words.json')
   .then(res => res.json())
   .then(data => {
     const today = new Date().toISOString().split('T')[0];
